a88afc6130e46ebea09417179b92a42b3a5c44f8
WemersonPD <wpdads10@gmail.com>
2021-05-19 22:17:13 -0300

    Add structure basic

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      name: "Navbar",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
