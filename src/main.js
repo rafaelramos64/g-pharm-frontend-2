1637a3f6791debbdb4af25cecb13d882664a0c48
Rafaelmaiden <rafaelmaggot64@gmail.com>
2021-05-20 23:42:56 -0300

    adusta

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import BootstrapVue from "bootstrap-vue";
 import VueSweetalert2 from "vue-sweetalert2";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
@@ -14,13 +14,12 @@ import "./services/localstorage";
 
 // Using Sweet alert 2
 import "sweetalert2/dist/sweetalert2.min.css";
+
 Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
 
 new Vue({
   router,
